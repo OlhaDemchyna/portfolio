71fc2768089415041fe6ec3774be0b3f0b379629
OlhaDemchyna <olyadem4ik@gmail.com>
2022-08-05 15:56:43 +0200

    Add Weather bootstrap react project

---
+++ weather-bootstrap-react/src/Components/Shell/Shell.jsx
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from 'react';
+
+import { useWeather, WeatherContext } from '../../../helpers';
+
+import * as classes from './Shell.module.css';
+
+function Shell({ children }) {
+    const contextValue = useWeather();
+
+    useEffect(() => {
+        contextValue.getTemperature('Kyiv')
+    }, [])
+
+    return (
+        <div className="container">
+            <div className={classes.weatherAppWrapper}>
+                <div className={classes.weatherApp}>
+                    <WeatherContext.Provider value={contextValue}>
+                        {children}
+                    </WeatherContext.Provider>
+                </div>
+                <small>
+                    Open-source code, by{' '}
+                    <a href="https://github.com/OlhaDemchyna" target="_blank">Olha Demchyna</a>
+                </small>
+            </div>
+        </div>
+    );
+}
+
+export default Shell;
